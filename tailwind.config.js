d582eb515110191886b45b00540c336f3b3e67f9
Albin Jaldevik <albja664@student.liu.se>
2025-12-08 00:00:33 +0000

    chore: fix tailwind bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './*.html'],
   darkMode: 'class',
-  plugins: [],
+  plugins: [typography],
   theme: {
     container: {
       center: true,
